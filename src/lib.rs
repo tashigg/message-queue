c4532191bf36c37a371904668832e61400acf434
Austin Bonander <austin.bonander@gmail.com>
2024-01-31 15:22:40 -0800

    chore: add CI config [TG-352]

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use color_eyre::eyre::eyre;
 use color_eyre::eyre::WrapErr;
 pub use color_eyre::eyre::{Error, Result};
+use tracing_subscriber::EnvFilter;
 
 use crate::args::LogFormat;
 
@@ -14,21 +15,19 @@ pub fn bootstrap(log_format: LogFormat) -> Result<()> {
         }
     }
 
-    // Enables capturing backtraces on stable
+    // Enables capturing backtraces on stable and adds color codes.
     color_eyre::install()?;
 
-    let builder = tracing_subscriber::fmt::fmt();
+    let builder = tracing_subscriber::fmt::fmt().with_env_filter(EnvFilter::from_default_env());
 
     match log_format {
-        LogFormat::Full => builder.try_init().map_err(|e| eyre!(e))?,
-        LogFormat::Compact => {
-            builder.compact().try_init().map_err(|e| eyre!(e))?;
-        }
-        LogFormat::Pretty => {
-            builder.pretty().try_init().map_err(|e| eyre!(e))?;
-        }
-        LogFormat::Json => builder.json().try_init().map_err(|e| eyre!(e))?,
+        // These all result in different typestate
+        LogFormat::Full => builder.try_init(),
+        LogFormat::Compact => builder.compact().try_init(),
+        LogFormat::Pretty => builder.pretty().try_init(),
+        LogFormat::Json => builder.json().try_init(),
     }
+    .map_err(|e| eyre!(e))?;
 
     Ok(())
 }
