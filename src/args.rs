2a883dc2e624d1de75c997e0df11b212d01d1a7d
Austin Bonander <austin.bonander@gmail.com>
2024-02-02 15:12:01 -0800

    feat: create basic main loop [TG-366]

--- src/args.rs
+++ src/args.rs
@@ -1,7 +1,12 @@
+use std::net::SocketAddr;
+
 #[derive(clap::Parser, Debug)]
 pub struct Args {
     #[clap(short, long, default_value = "full")]
     pub log: LogFormat,
+
+    #[clap(short = 'L', long, default_value = "0.0.0.0:1883")]
+    pub listen_addr: SocketAddr,
 }
 
 #[derive(clap::ValueEnum, Debug, Copy, Clone)]
