bc3883c441bfbb92adae66c079a6233de56f5900
Steven Joruk <steven@joruk.com>
2024-02-14 21:15:58 +0000

    Add rumqttd's router (#13)
    * chore: add rumqttd's router
    
    This is a separate crate because rumqttd often imports using absolute
    paths from `crate::`, even for descendant modules. It would have
    required many more changes to rumqttd to make them relative.
    
    * refactor: rename `rumqttd-router` to `rumqttd-shim`, move `protocol` there
    
    ---------
    
    Co-authored-by: Austin Bonander <austin@launchbadge.com>

---
+++ rumqttd-shim/src/lib.rs
@@ -0,0 +1,36 @@
+//! Source re-exports from the `rumqttd` crate to bypass coherence.
+//!
+//! If the files don't exist or are outdated, try `git submodule update --init --recursive`.
+//!
+//! Docs (for public items, anyway): https://docs.rs/rumqttd/latest/rumqttd/index.html
+//!
+//! This should save us having to write our own MQTT handling code.
+//! `rumqttd` is published as a crate but many items are crate-private that
+//! need to be accessible for any alternative broker implementation.
+//! It also brings in a ton of non-optional crates that we don't strictly need.
+//!
+//! Additional impls for types in `rumqttd` may be added here,
+//! such as getters for private fields.
+
+// `rumqttd::protocol` which *is* public, but its packet types have crate-private fields
+// that we'll need to add getters for.
+#[path = "../../rumqtt/rumqttd/src/protocol/mod.rs"]
+#[allow(warnings)]
+pub mod protocol;
+
+// `rumqttd::router`, which is not public in the upstream source.
+#[path = "../../rumqtt/rumqttd/src/router/mod.rs"]
+#[allow(warnings)]
+pub mod router;
+
+#[path = "../../rumqtt/rumqttd/src/segments/mod.rs"]
+#[allow(warnings)]
+pub mod segments;
+
+pub use router::{shared_subs::Strategy, Router, RouterConfig};
+
+use router::{ConnectionId, Filter, Notification, RouterId, Topic};
+use segments::Storage;
+
+type Cursor = (u64, u64);
+type Offset = (u64, u64);
--- src/mqtt/mod.rs
+++ src/mqtt/mod.rs
@@ -1,27 +1,3 @@
 pub mod broker;
 
-/// `rumqttd::protocol`
-///
-/// If the files don't exist, try `git submodule update --init --recursive`.
-///
-/// Docs: https://docs.rs/rumqttd/latest/rumqttd/protocol/index.html
-///
-/// This should save us having to write our own protocol codec.
-/// `rumqttd` is published as a crate but all of the packet types have crate-private fields that
-/// need to be accessible for any alternative broker implementation.
-/// It also brings in a ton of non-optional crates that we don't strictly need.
-///
-/// TODO: upstream getters for private fields we need access to, and we can get rid of the submodule
-#[path = "../../rumqtt/rumqttd/src/protocol/mod.rs"]
-#[allow(warnings)]
-pub mod protocol;
-
-// Other potentially useful modules:
-// `routing`, `segments`
-//
-// We may eventually want to just use `rumqttd` as a crate anyway, like we can just use it *as*
-// our local MQTT broker implementation and then we just forward messages to our TCE network.
-//
-// However, for QoS1 and QoS2 packets, we probably don't want to respond to the sender
-// with `PUBACK` and `PUBREL` respectively until we have some notion that the message has actually
-// been acknowledged by other peers. Which means we need a lot of access to the broker internals.
+pub use rumqttd_shim::{protocol, router};
