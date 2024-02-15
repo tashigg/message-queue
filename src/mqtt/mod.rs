5ba83fcd5b8ae1c2a89cdba6ba86e9a77f4df4ea
Steven Joruk <steven@joruk.com>
2024-02-15 11:26:31 +0000

    feat: support sessions

--- src/mqtt/mod.rs
+++ src/mqtt/mod.rs
@@ -1,3 +1,7 @@
-pub mod broker;
-
 pub use rumqttd_shim::{protocol, router};
+
+pub mod broker;
+mod session;
+
+// It's a String to match rumqtt's type, but it could be [u8; 23] if we wanted.
+type ClientId = String;
