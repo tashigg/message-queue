c722e1f6ae04f8ed7142c397bf03bdfc5b0772e1
Austin Bonander <austin.bonander@gmail.com>
2024-02-21 01:04:12 -0800

    feat: handle `Publish` and ack properly [TG-371]

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,4 +1,4 @@
-use color_eyre::eyre::WrapErr;
+use color_eyre::eyre::{eyre, WrapErr};
 use serde::de::DeserializeOwned;
 use std::path::Path;
 use std::{fs, io};
@@ -8,11 +8,26 @@ pub mod addresses;
 pub mod users;
 
 fn read_toml<T: DeserializeOwned>(name: &str, path: &Path) -> crate::Result<T> {
+    read_toml_optional(name, path)?.ok_or_else(|| {
+        eyre!(
+            "error reading {name} from {}: file not found",
+            path.display()
+        )
+    })
+}
+
+fn read_toml_optional<T: DeserializeOwned>(name: &str, path: &Path) -> crate::Result<Option<T>> {
     let config_toml = if path == Path::new("-") {
-        io::read_to_string(io::stdin().lock()).wrap_err("error reading from stdin")?
+        io::read_to_string(io::stdin().lock())
+            .wrap_err_with(|| format!("error reading {name} from stdin"))?
     } else {
-        fs::read_to_string(path)
-            .wrap_err_with(|| format!("error reading from {}", path.display()))?
+        match fs::read_to_string(path) {
+            Ok(s) => s,
+            Err(e) if e.kind() == io::ErrorKind::NotFound => return Ok(None),
+            Err(e) => {
+                return Err(eyre!(e).wrap_err(format!("error reading from {}", path.display())))
+            }
+        }
     };
 
     toml::from_str(&config_toml)
--- src/mqtt/mod.rs
+++ src/mqtt/mod.rs
@@ -1,7 +1,23 @@
+//! Our MQTT broker implementation.
+//!
+//! Currently, the only version of MQTT we plan to support is version 5.
+//!
+//! The protocol specification for MQTT v5 can be found at:
+//! https://docs.oasis-open.org/mqtt/mqtt/v5.0/os/mqtt-v5.0-os.html
 pub use rumqttd_shim::{protocol, router};
 
 pub mod broker;
 mod session;
 
+mod publish;
+
 // It's a String to match rumqtt's type, but it could be [u8; 23] if we wanted.
 type ClientId = String;
+
+/// Max length of a UTF-8 string allowed by the MQTT spec.
+///
+/// This is a hard limit as the protocol encoding of UTF-8 strings uses a fixed-width 2-byte prefix.
+/// MQTT structures containing strings should NEVER be constructed with a string longer than this.
+///
+/// https://docs.oasis-open.org/mqtt/mqtt/v5.0/os/mqtt-v5.0-os.html#_Toc3901010
+pub const MAX_STRING_LEN: usize = 65535;
